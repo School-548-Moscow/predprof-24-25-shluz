6f61daae0589de9be1ee91d8b84ac3a2e428cbf6
School-548-Moscow <125913808+School-548-Moscow@users.noreply.github.com>
2025-02-10 15:36:41 +0300

    Delete com_test.py

--- com_test.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-try:
-    arduino = serial.Serial('COM3', 9600, timeout=1)
-    print("Успешно подключено!")
-    arduino.close()
-except serial.SerialException as e:
-    print(f"Ошибка: {e}")
